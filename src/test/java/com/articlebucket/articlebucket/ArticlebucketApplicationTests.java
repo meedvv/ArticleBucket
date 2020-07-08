d2329a7d3c6bed892c1dfd0d0953077d095b4571
Alexandr Medvedev <alexandr.medvedev.379@gmail.com>
2020-07-08 15:39:06 +0300

    Setup Project

---
+++ src/test/java/com/articlebucket/articlebucket/ArticlebucketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.articlebucket.articlebucket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArticlebucketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
