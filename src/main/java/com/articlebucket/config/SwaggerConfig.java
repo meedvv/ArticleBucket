64f5b036b13cc06dcf4d71c5367d832c43ada5a6
Alexandr Medvedev <alexandr.medvedev.379@gmail.com>
2020-07-10 11:54:23 +0300

    Setting Up Swagger 2

---
+++ src/main/java/com/articlebucket/config/SwaggerConfig.java
@@ -0,0 +1,27 @@
+package com.articlebucket.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import static springfox.documentation.builders.PathSelectors.any;
+import static springfox.documentation.spi.DocumentationType.SWAGGER_2;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+
+    private static final String CONTROLLER_BASE_PACKAGE = "com.articlebucket.web.controller";
+
+    @Bean
+    public Docket productApi() {
+        return new Docket(SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage(CONTROLLER_BASE_PACKAGE))
+                .paths(any())
+                .build();
+    }
+
+}
